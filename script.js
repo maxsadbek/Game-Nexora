9e34e63ea1d76ba73555f4b8536720dfb47b7520
maxsad_bek <masadanb@gmail.com>
2025-08-25 14:16:12 +0500

    add

--- script.js
+++ script.js
@@ -20,22 +20,38 @@ document.querySelector(".check").addEventListener("click", function () {
         body.style.backgroundColor = "a60b347"
         document.querySelector(".number").textContent = secretNumber;
         document.querySelector(".number").style.width = "20rem"
+        if (score > highScore) {
+            highScore = score
+            document.querySelector(".highscore").textContent = highScore
+        }
     }
 
     else if (guess !== secretNumber) {
         if (score > 1) {
             score--
             document.querySelector(".score").textContent = score
-            displayMessage(guess > secretNumber?"📈To high!":"📉To low!")
+            displayMessage(guess > secretNumber ? "📈To high!" : "📉To low!")
         }
-        else{
+        else {
             displayMessage("You losy the game")
+            displayMessage.querySelector(".score").textContent = 0
         }
     }
 
     else if (guess <= 0) {
         displayMessage("Number must be > 0");
     }
-
 })
 
+
+document.querySelector(".again").addEventListener("click", function(){
+    score = 20
+    highScore = 0
+    secretNumber= Math.trunc(Math.random() * 20 + 1)
+
+    document.querySelector(".guess").value = ""
+    document.querySelector(".score").textContent = score
+    document.querySelector(".highscore").textContent = highScore
+    document.querySelector(".number").style.width = "16rem"
+    document.querySelector(".number").textContent = "?";
+ })
